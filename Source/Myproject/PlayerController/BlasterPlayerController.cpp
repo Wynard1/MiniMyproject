16ceb12053fb103d33bd96cc7d4b09f8aba1d7c3
Wynyard <1697570085@qq.com>
2023-12-28 10:11:22 +0800

    9.1 对局时间倒计时 & 9.2 客户端同步更新时间

--- Source/Myproject/PlayerController/BlasterPlayerController.cpp
+++ Source/Myproject/PlayerController/BlasterPlayerController.cpp
@@ -15,6 +15,30 @@ void ABlasterPlayerController::BeginPlay()
 	BlasterHUD = Cast<ABlasterHUD>(GetHUD());
 }
 
+void ABlasterPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	SetHUDTime(); 
+	
+	//每一tick，调用check同步时间的函数
+	CheckTimeSync(DeltaTime);
+}
+
+void ABlasterPlayerController::CheckTimeSync(float DeltaTime)
+{
+	//未更新时间。每个tick增加直到超过上限
+	TimeSyncRunningTime += DeltaTime;
+
+	if (IsLocalController() && TimeSyncRunningTime > TimeSyncFrequency)
+	{
+		ServerRequestServerTime(GetWorld()->GetTimeSeconds());
+
+		//更新后清0
+		TimeSyncRunningTime = 0.f;
+	}
+}
+
 void ABlasterPlayerController::OnPossess(APawn* InPawn)
 {
 	Super::OnPossess(InPawn);
@@ -99,7 +123,6 @@ void ABlasterPlayerController::SetHUDWeaponAmmo(int32 Ammo)
 	}
 }
 
-
 void ABlasterPlayerController::SetHUDCarriedAmmo(int32 Ammo)
 {
 	BlasterHUD = BlasterHUD == nullptr ? Cast<ABlasterHUD>(GetHUD()) : BlasterHUD;
@@ -110,5 +133,73 @@ void ABlasterPlayerController::SetHUDCarriedAmmo(int32 Ammo)
 	{
 		FString AmmoText = FString::Printf(TEXT("%d"), Ammo);
 		BlasterHUD->CharacterOverlay->CarriedAmmoAmount->SetText(FText::FromString(AmmoText));
+	}
+}
+
+void ABlasterPlayerController::SetHUDMatchCountdown(float CountdownTime)
+{
+	BlasterHUD = BlasterHUD == nullptr ? Cast<ABlasterHUD>(GetHUD()) : BlasterHUD;
+	bool bHUDValid = BlasterHUD &&
+		BlasterHUD->CharacterOverlay &&
+		BlasterHUD->CharacterOverlay->MatchCountdownText;
+
+	if (bHUDValid)
+	{
+		//设置分和秒
+		int32 Minutes = FMath::FloorToInt(CountdownTime / 60.f);
+		int32 Seconds = CountdownTime - Minutes * 60;
+		
+		//字符打印(%02d:将数字格式化为两个数字，左边填充为0)
+		FString CountdownText = FString::Printf(TEXT("%02d:%02d"), Minutes, Seconds);
+		BlasterHUD->CharacterOverlay->MatchCountdownText->SetText(FText::FromString(CountdownText));
+	}
+}
+
+void ABlasterPlayerController::SetHUDTime()
+{
+	//计算当前时间：从游戏开始到现在所经过的时间将从MatchTime中减去
+	uint32 SecondsLeft = FMath::CeilToInt(MatchTime - GetServerTime());
+	
+	//当剩余时间不等于更新后的剩余时间时，更新要显示的剩余时间
+	if (CountdownInt != SecondsLeft)
+	{
+		SetHUDMatchCountdown(MatchTime - GetServerTime());
+	}
+
+	//显示部分更新完毕，剩余时间更新以进行下一秒的比较
+	CountdownInt = SecondsLeft;
+}
+
+void ABlasterPlayerController::ServerRequestServerTime_Implementation(float TimeOfClientRequest)
+{
+	//在服务器上，服务器会得到它自己的当前时间
+	float ServerTimeOfReceipt = GetWorld()->GetTimeSeconds();
+
+	//通过Client RPC调用把这个传递回来
+	ClientReportServerTime(TimeOfClientRequest, ServerTimeOfReceipt);
+}
+
+void ABlasterPlayerController::ClientReportServerTime_Implementation(float TimeOfClientRequest, float TimeServerReceivedClientRequest)
+{
+	float RoundTripTime = GetWorld()->GetTimeSeconds() - TimeOfClientRequest;
+	float CurrentServerTime = TimeServerReceivedClientRequest + (0.5f * RoundTripTime);
+	ClientServerDelta = CurrentServerTime - GetWorld()->GetTimeSeconds();
+}
+
+float ABlasterPlayerController::GetServerTime()
+{
+	//服务端上 ：本地时间
+	if (HasAuthority()) return GetWorld()->GetTimeSeconds();
+
+	//客户端上 ：本地时间+修正
+	else return GetWorld()->GetTimeSeconds() + ClientServerDelta;
+}
+
+void ABlasterPlayerController::ReceivedPlayer()
+{
+	Super::ReceivedPlayer();
+	if (IsLocalController())
+	{
+		ServerRequestServerTime(GetWorld()->GetTimeSeconds());
 	}
 }
--- Source/Myproject/PlayerController/BlasterPlayerController.h
+++ Source/Myproject/PlayerController/BlasterPlayerController.h
@@ -26,13 +26,51 @@ public:
 
 	void SetHUDCarriedAmmo(int32 Ammo);
 
+	void SetHUDMatchCountdown(float CountdownTime);
+
 	//重生时更换Pawn会调用
 	virtual void OnPossess(APawn* InPawn) override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual float GetServerTime(); // Synced with server world clock
+
+	virtual void ReceivedPlayer() override; // Sync with server clock as soon as possible
 protected:
 	virtual void BeginPlay() override;
+
+	void SetHUDTime();
+
+	/**
+	* Sync time between client and server
+	*/
+
+	// Requests the current server time, passing in the client's time when the request was sent
+	UFUNCTION(Server, Reliable)
+	void ServerRequestServerTime(float TimeOfClientRequest);
+
+	// Reports the current server time to the client in response to ServerRequestServerTime
+	UFUNCTION(Client, Reliable)
+	void ClientReportServerTime(float TimeOfClientRequest, float TimeServerReceivedClientRequest);
+
+	float ClientServerDelta = 0.f; // difference between client and server time
+
+	//更新频次，未更新时间超过此就执行更新
+	UPROPERTY(EditAnywhere, Category = Time)
+	float TimeSyncFrequency = 5.f;
+
+	//未更新的时间。每次tick增加delta time
+	float TimeSyncRunningTime = 0.f;
+
+	void CheckTimeSync(float DeltaTime);
 
 private:
 	UPROPERTY()
 	class ABlasterHUD* BlasterHUD;
 
+	//初始时间(总对局时间)
+	float MatchTime = 120.f;
+
+	//剩余时间
+	uint32 CountdownInt = 0;
 };
